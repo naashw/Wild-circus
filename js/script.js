8b19efeabc92f9d1c7eb826a3611f24b894e85dc
Naash <dgmg@outlook.fr>
2018-01-16 07:28:40 +0100

    Final?
    dev 6.0

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
   $('.js-scrollTo').on('click', function() { // Au clic sur un élément
     var page = $(this).attr('href'); // Page cible
     var speed = 750; // Durée de l'animation (en ms)
-    $('html, body').animate( { scrollTop: $(page).offset().top }, speed ); // Go
+    $('html, body').animate( { scrollTop: $(page).offset().top - 36 }, speed ); // Go
     return false;
   });
 });
